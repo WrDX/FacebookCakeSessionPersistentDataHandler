e362b0fc36f2a931ab716c7c22ca774a96d671c6
Wouter <WrDX@users.noreply.github.com>
2018-02-26 23:34:05 +0100

    Added $enableSessionCheck

--- src/FacebookCakeSessionPersistentDataHandler.php
+++ src/FacebookCakeSessionPersistentDataHandler.php
@@ -8,15 +8,33 @@ namespace WrDX\Facebook;
 class FacebookCakeSessionPersistentDataHandler implements \Facebook\PersistentData\PersistentDataInterface {
 
     /**
-     * @var string
+     * @var string Prefix to use for session variables.
      */
     protected $sessionPrefix = null;
+    
+    /**
+    * Init the session handler
+    *
+    * @param boolean $enableSessionCheck
+    * @param string  $sessionPrefix
+    *
+    * @throws FacebookSDKException
+    */
+    public function __construct($enableSessionCheck = true, $sessionPrefix = 'Facebook.php-graph-sdk') {
 
-    function __construct($sessionPrefix = 'Facebook.php-graph-sdk') {
+        # CakeSession::start() will determine if Session has been started. 
+        # If not, it will start the Session
+        if ($enableSessionCheck && ! \CakeSession::start()) {
+            throw new FacebookSDKException('Sessions are not active.', 720);
+        }
+
         $this->sessionPrefix = $sessionPrefix . '.';
+
     }
 
     /**
+     * Get a value from a persistent data store
+     *
      * @param string $key
      *
      * @return mixed
@@ -26,10 +44,13 @@ class FacebookCakeSessionPersistentDataHandler implements \Facebook\PersistentDa
     }
 
     /**
+     * Set a value in the persistent data store
+     *
      * @param string $key
      * @param mixed  $value
      */
     public function set($key, $value) {
         \CakeSession::write($this->sessionPrefix . $key, $value);
     }
+
 }
